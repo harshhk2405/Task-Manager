e7c733c83e9de630c1d2e432e6068e4076730ccb
harshhk2405 <146757500+harshhk2405@users.noreply.github.com>
2024-06-18 01:28:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+
+app.use(express.json());
+app.use(cors());
+
+const mongoUrl = 'mongodb+srv://harshhk2405:8ssu32oEahOdoWcm@myapi.3ql1wfc.mongodb.net/?retryWrites=true&w=majority&appName=MyApi';
+mongoose.connect(mongoUrl,(err)=>{
+  // useNewUrlParser: "true",
+  console.log(("mongo db connected!"));
+  console.log(err);
+  // useUnifiedTopology: "true"
+})
+mongoose.connection.on("error", err => {
+
+  console.log("err", err)
+
+})
+mongoose.connection.on(mongoUrl, err => {
+  
+  console.log("Mongodb connected...");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/profile", profileRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
+}
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Backend is running on port ${port}`);
+});
